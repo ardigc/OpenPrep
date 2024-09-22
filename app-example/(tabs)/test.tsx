38ca79a465da2f4c1d42977430771c0a5c3be7d1
ardigc <agordo.95@gmail.com>
2024-09-22 18:30:11 +0200

    moce to example

---
+++ app-example/(tabs)/test.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Test (){
+return(
+    <View>
+        <Text>
+        This is my fisrt tab
+        </Text>
+    </View>
+)
+}
